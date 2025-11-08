3f2055308afd3da70707baf1b769f7af493c85c5
Bhsd <2545473905@qq.com>
2025-11-08 23:26:45 +0800

    feat(code-mirror): check wiki-editor plugin

--- packages/code-mirror/src/index.ts
+++ packages/code-mirror/src/index.ts
@@ -3,10 +3,11 @@ import { defineIPEPlugin } from '~~/defineIPEPlugin.js'
 export default defineIPEPlugin({
   name: 'code-mirror-v6',
   apply: (ctx) => {
+    ctx.set('plugin:code-mirror-v6', true)
     ctx.on(
       'quick-edit/wiki-page',
       async ({ modal, wikiPage: { contentmodel, ns, title } }) => {
-        ;(
+        ;const cm = await (
           await import(
             // @ts-ignore
             /* @vite-ignore */ 'https://cdn.jsdelivr.net/npm/@bhsd/codemirror-mediawiki/dist/mw.min.js'
@@ -17,6 +18,9 @@ export default defineIPEPlugin({
           ns,
           title
         )
+        if (ctx.get('plugin:wiki-editor')) {
+          cm.prefer({wikiEditor: true})
+        }
       }
     )
   },
