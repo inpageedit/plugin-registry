f1a00fab10647db749e2445b01f024d05c7aabd6
dragon-fish <dragon-fish@qq.com>
2025-11-08 23:41:12 +0800

    feat: resolve conflicts

--- packages/code-mirror/src/index.ts
+++ packages/code-mirror/src/index.ts
@@ -1,26 +1,42 @@
 import { defineIPEPlugin } from '~~/defineIPEPlugin.js'
 
+declare module '@inpageedit/core' {
+  export interface InPageEdit {
+    'plugin:code-mirror-v6': boolean
+  }
+}
+
 export default defineIPEPlugin({
   name: 'code-mirror-v6',
   apply: (ctx) => {
     ctx.set('plugin:code-mirror-v6', true)
+
+    let preferWikiEditor = false
+    ctx.inject(['plugin:wiki-editor'], () => {
+      preferWikiEditor = true
+      for (let [_, scope] of ctx.registry.entries()) {
+        if (scope.name === 'wiki-editor') {
+          scope.dispose()
+          break
+        }
+      }
+    })
+
     ctx.on(
       'quick-edit/wiki-page',
       async ({ modal, wikiPage: { contentmodel, ns, title } }) => {
-        ;const cm = await (
-          await import(
-            // @ts-ignore
-            /* @vite-ignore */ 'https://cdn.jsdelivr.net/npm/@bhsd/codemirror-mediawiki/dist/mw.min.js'
-          )
-        ).CodeMirror.fromTextArea(
+        const pkg = await import(
+          // @ts-ignore
+          /* @vite-ignore */ 'https://cdn.jsdelivr.net/npm/@bhsd/codemirror-mediawiki/dist/mw.min.js'
+        )
+        const { CodeMirror } = pkg
+        const cm = CodeMirror.fromTextArea(
           modal.get$content().querySelector('textarea[name="text"]')!,
           contentmodel,
           ns,
           title
         )
-        if (ctx.get('plugin:wiki-editor')) {
-          cm.prefer({wikiEditor: true})
-        }
+        cm.prefer({ wikiEditor: preferWikiEditor })
       }
     )
   },
--- packages/wiki-editor/src/index.ts
+++ packages/wiki-editor/src/index.ts
@@ -9,14 +9,17 @@ declare global {
   }
 }
 
+declare module '@inpageedit/core' {
+  export interface InPageEdit {
+    'plugin:wiki-editor': boolean
+  }
+}
+
 export default defineIPEPlugin({
   name: 'wiki-editor',
   apply(ctx) {
     ctx.set('plugin:wiki-editor', true)
     ctx.on('quick-edit/wiki-page', async (payload) => {
-      if (ctx.get('plugin:code-mirror-v6')) {
-        return
-      }
       const textarea = payload.modal
         .get$content()
         .querySelector<HTMLTextAreaElement>('textarea[name="text"]')!
