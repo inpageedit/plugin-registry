b643bb600dac98dcedeaefebc3de936ebfbc8142
Bhsd <2545473905@qq.com>
2025-11-08 23:12:27 +0800

    fix(wiki-editor): skip if plugin:code-mirror-v6 is enabled

--- packages/wiki-editor/src/index.ts
+++ packages/wiki-editor/src/index.ts
@@ -14,6 +14,9 @@ export default defineIPEPlugin({
   apply(ctx) {
     ctx.set('plugin:wiki-editor', true)
     ctx.on('quick-edit/wiki-page', async (payload) => {
+      if (ctx.get('plugin:code-mirror-v6')) {
+        return
+      }
       const textarea = payload.modal
         .get$content()
         .querySelector<HTMLTextAreaElement>('textarea[name="text"]')!
